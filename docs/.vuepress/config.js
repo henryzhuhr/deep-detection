712395075ed0578c4f9c9893f1c1e783373a597c
Henry Zhu <38650110+HenryZhuHR@users.noreply.github.com>
2021-12-25 20:39:57 +0800

    config

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,19 +2,12 @@ module.exports = {
     // 站点配置
     base: '/ObjectDetection/',
     lang: 'zh-CN',
-    title: '目标检测',
-    description: '目标检测算法从模型训练到终端部署',
-
+    title: '你好， VuePress ！',
+    description: '这是我的第一个 VuePress 站点',
+  
     // 主题和它的配置
     theme: '@vuepress/theme-default',
     themeConfig: {
-        logo: 'https://vuejs.org/images/logo.png',
-        lastUpdated: 'Last Updated', // 文档更新时间：每个文件git最后提交的时间
-        nav : [
-            { text: '接口定义', link: '/apiword' },
-            { text: '接口字段定义', link: '/api' },
-            { text: '附录：错误码', link: '/error' }
-        ],
+      logo: 'https://vuejs.org/images/logo.png',
     },
-
-}
+  }
